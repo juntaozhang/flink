4964f8052e88cf984b34febfa39aa9550fc11fc3
Juntao Zhang <juntzhang@ebay.com>
2025-04-06 09:05:12 +0800

    fix PTFs INTERVAL arg

--- flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/nodes/exec/stream/ProcessTableFunctionTestUtils.java
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/nodes/exec/stream/ProcessTableFunctionTestUtils.java
@@ -30,6 +30,7 @@ import org.apache.flink.table.test.program.SourceTestStep;
 import org.apache.flink.types.ColumnList;
 import org.apache.flink.types.Row;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 
@@ -225,6 +226,13 @@ public class ProcessTableFunctionTestUtils {
     public static class EmptyArgFunction extends TestProcessTableFunctionBase {
         public void eval() {
             collectObjects("empty");
+        }
+    }
+
+    /** Testing function. */
+    public static class IntervalArgFunction extends TestProcessTableFunctionBase {
+        public void eval(@DataTypeHint Duration d) {
+            collectObjects(d);
         }
     }
 
